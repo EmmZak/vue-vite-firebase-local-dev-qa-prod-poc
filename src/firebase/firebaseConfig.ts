da90555484779b825ffbd521f8aa5590ad72e30b
emmzak <emmanuelzakaryan@gmail.com>
2024-02-23 22:58:48 +0100

    add local env file and emulator

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,10 +1,32 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 import firebaseConfig from "../config/dev/firebaseConfig";
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+let app = initializeApp(firebaseConfig);
+let db 
+let auth 
+let rtDb
+
+const ENV = import.meta.env.MODE
+
+if (ENV === "development") {
+    console.log("ENV", ENV)
+
+    db = getFirestore();
+    connectFirestoreEmulator(db, '127.0.0.1', import.meta.env.VITE_FIRESTORE_EMULATOR_PORT);
+
+    auth = getAuth();
+    connectAuthEmulator(auth, "http://127.0.0.1:" + import.meta.env.VITE_AUTH_EMULATOR_PORT);
+
+    rtDb = getDatabase();
+    connectDatabaseEmulator(rtDb, "127.0.0.1", import.meta.env.VITE_DATABASE_EMULATOR_PORT);
+} else {
+    db = getFirestore(app)
+    auth = getAuth(app)
+}
 
 export {
-    db
+    db, auth, rtDb
 }
